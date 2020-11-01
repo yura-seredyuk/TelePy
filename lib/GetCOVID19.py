8f8751bd90c3a6bb21504b841ef33e74ec48e95e
Yurii <yura2590@gmail.com>
2020-11-01 16:35:52 +0200

    Add submenu for global info

--- lib/GetCOVID19.py
+++ lib/GetCOVID19.py
@@ -12,7 +12,6 @@ class GetCOVID19:
             user=USERNAME,
             password=PASSWORD
         )
-        # self.__template = "{0:8}|{1:10}|{2:15}|{3:7}|{4:10}"
         self.__cursor = self.__db.cursor()
         self.__cursor.execute("CREATE DATABASE IF NOT EXISTS telepy")
         self.__cursor.execute("USE telepy")
@@ -20,14 +19,11 @@ class GetCOVID19:
             "CREATE TABLE IF NOT EXISTS Countries (id INT AUTO_INCREMENT PRIMARY KEY, Country VARCHAR(64), CountryCode VARCHAR(3), Slug VARCHAR(128), NewConfirmed INT, TotalConfirmed INT, NewDeaths INT, TotalDeaths INT, NewRecovered INT, TotalRecovered INT, Date VARCHAR(128))")
         self.__cursor.execute(
             "CREATE TABLE IF NOT EXISTS Global (NewConfirmed INT, TotalConfirmed INT, NewDeaths INT, TotalDeaths INT, NewRecovered INT, TotalRecovered INT)")
-        # self.__add_info()
         self.__get_covid19_info()
 
     def __get_covid19_info(self):
-        # print("Inside")
         responce = requests.get(self.__COVID19API)
         covid_data = responce.json()
-        # print("API => ", covid_data)
         self.__cursor.execute("TRUNCATE Countries")
         for item in covid_data['Countries']:
             sql = "INSERT INTO Countries (Country, CountryCode, Slug, NewConfirmed, TotalConfirmed, NewDeaths, TotalDeaths, NewRecovered, TotalRecovered, Date) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
@@ -55,7 +51,7 @@ class GetCOVID19:
             elif choice == 3:
                 self.sort_by_country_name()
             elif choice == 4:
-                self.show_global_info()
+                self.sub_menu_global()
             elif choice == 0:
                 exit = True
                 print("Bye!")
@@ -99,6 +95,31 @@ class GetCOVID19:
         for item in self.__cursor.fetchall():
             print(template.format(*item))
 
+    def sub_menu_global(self):
+        sub_exit = False
+        while not sub_exit:
+            choice = int(input(
+                "\t1. All information\n\t2. New confirmed\n\t3. Total confirmed\n\t4. New deaths\n\t5. Total deaths\n\t6. New recovered\n\t7. Total recovered\n\t0 Exit\n\t ===>> "))
+            if choice == 1:
+                self.show_global_info()
+            elif choice == 2:
+                self.show_global_item("NewConfirmed")
+            elif choice == 3:
+                self.show_global_item("TotalConfirmed")
+            elif choice == 4:
+                self.show_global_item("NewDeaths")
+            elif choice == 5:
+                self.show_global_item("TotalDeaths")
+            elif choice == 6:
+                self.show_global_item("NewRecovered")
+            elif choice == 7:
+                self.show_global_item("TotalRecovered")
+            elif choice == 0:
+                sub_exit = True
+                print("Bye!")
+            else:
+                print("Wrong choice.")
+
     def show_global_info(self):
         self.__cursor.execute(
             "SELECT NewConfirmed, TotalConfirmed, NewDeaths, TotalDeaths, NewRecovered, TotalRecovered FROM Global")
@@ -110,3 +131,13 @@ class GetCOVID19:
                                 for d in self.__cursor.description]))
         template = "|{0:_^16}|{1:_^16}|{2:_^11}|{3:_^13}|{4:_^14}|{5:_^16}|"
         print(template.format(*self.__cursor.fetchone()))
+
+    def show_global_item(self, item):
+        self.__cursor.execute(
+            f"SELECT {item} FROM Global")
+        for i in range(1, len(item)):
+            if item[i].isupper():
+                break
+        item = item.replace(item[i], ' '+item[i])
+        print("Global information "+item+": ",
+              ' '.join(map(str, self.__cursor.fetchone())))
